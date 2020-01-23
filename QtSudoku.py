21268d738c392a9061561596358f719f1a1bf192
Jon Moore <jonr.moore75@gmail.com>
2020-01-23 23:28:43 +0000

    Minor Changes
    - Cell stores grid location
    - Explicitly pass self as parent of board
    - Window bigger and location set in code
    - Moved board to outer scope ie file main
    - High DPI code added but not very effective

--- QtSudoku.py
+++ QtSudoku.py
@@ -3,15 +3,28 @@ from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtWidgets import QMainWindow, QLabel, QGridLayout, QWidget
 from PyQt5.QtCore import QSize    
 
-from PyQt5.QtGui import QPainter, QBrush, QPen, QCursor, QFont
+from PyQt5.QtGui import QFont#, QPainter, QBrush, QPen, QCursor
 
+from os import environ
+
+if hasattr(QtCore.Qt, 'AA_EnableHighDpiScaling'):
+    QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
+
+if hasattr(QtCore.Qt, 'AA_UseHighDpiPixmaps'):
+    QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
+    
+environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
+    
 class Cell(QLabel):
-    def __init__(self, value, font, parent):
+    def __init__(self, parent, value, font, i, j):
         self.cellString = str(value) if value is not 0 else ' '
         super(QLabel, self).__init__(self.cellString, parent)
+        
         self.setStyleSheet("background-color: white;")
         self.setAlignment(QtCore.Qt.AlignCenter)
         self.setFont(font)
+        self.i = i
+        self.j = j
         
     def mouseReleaseEvent(self, QMouseEvent):
         print 'Clicked on '+self.cellString
@@ -20,7 +33,7 @@ class SudokuMainWindow(QMainWindow):
     def __init__(self, board):
         super(QMainWindow, self).__init__()
  
-        self.setFixedSize(QSize(500, 500))    
+        self.setGeometry(500, 30, 900, 900)    
         self.setWindowTitle("Simple Sudoku") 
         self.setStyleSheet("background-color: grey;")
         
@@ -28,45 +41,41 @@ class SudokuMainWindow(QMainWindow):
         self.setCentralWidget(centralWidget)   
  
         gridLayout = QGridLayout()     
+#        gridLayout.setVerticalSpacing(2)
+#        gridLayout.setHorizontalSpacing(2)
         centralWidget.setLayout(gridLayout)  
         
-        self.CreateBoard(board, gridLayout)
-       
-#        menu = self.menuBar().addMenu('File')
-#        action = menu.addAction('Quit')
-#        action.triggered.connect(QtWidgets.QApplication.quit)
+        self.CreateBoard(board, self, gridLayout)
         
-    def CreateBoard(self, board, gridLayout):
-        cellfont = QFont("Arial", 25, QFont.Bold) 
+    def CreateBoard(self, board, parent, layout):
+        cellfont = QFont("Arial", 45, QFont.Bold) 
         for i in range(0,9):
             for j in range(0,9):
-                cellLabel = Cell(board[i][j], cellfont, self)
-                gridLayout.addWidget(cellLabel, i, j) 
+                cellLabel = Cell(parent, board[i][j], cellfont, i, j)
+                layout.addWidget(cellLabel, i, j) 
         
     def mouseReleaseEvent(self, QMouseEvent):
-        my = self.menuBar().height()
-        print('('+str(QMouseEvent.x())+', '+str(QMouseEvent.y()-my)+') \
+        print('('+str(QMouseEvent.x())+', '+str(QMouseEvent.y())+') \
               ('+str(self.width())+','+str(self.height())+')')
+        
+def run_app(board): 
+    app = QtWidgets.QApplication(sys.argv)
+    mainWin = SudokuMainWindow(board)
+    mainWin.show()
+    return app.exec_()
 
                 
-if __name__ == "__main__":
-    def run_app():
-        
-        board = [
-        [7,8,0,4,0,0,1,2,0],
-        [6,0,0,0,7,5,0,0,9],
-        [0,0,0,6,0,1,0,7,8],
-        [0,0,7,0,4,0,2,6,0],
-        [0,0,1,0,5,0,9,3,0],
-        [9,0,4,0,6,0,0,0,5],
-        [0,7,0,3,0,0,0,1,2],
-        [1,2,0,0,0,7,4,0,0],
-        [0,4,9,2,0,6,0,0,7]
-        ]
-        
-        app = QtWidgets.QApplication(sys.argv)
-        mainWin = SudokuMainWindow(board)
-        mainWin.show()
-        return app.exec_()
-
-    sys.exit(run_app())
+if __name__ == "__main__":  
+    board = [
+    [7,8,0,4,0,0,1,2,0],
+    [6,0,0,0,7,5,0,0,9],
+    [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
+    [0,0,1,0,5,0,9,3,0],
+    [9,0,4,0,6,0,0,0,5],
+    [0,7,0,3,0,0,0,1,2],
+    [1,2,0,0,0,7,4,0,0],
+    [0,4,9,2,0,6,0,0,7]
+    ]
+    
+    sys.exit(run_app(board))
