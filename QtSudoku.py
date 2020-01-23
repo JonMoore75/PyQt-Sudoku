8f56a3e293c2eed1d9d548504a39842c5453b4e2
Jon Moore <jonr.moore75@gmail.com>
2020-01-23 18:08:40 +0000

    Better Board Display
    Now uses a 2d list of integers as the board, with 0 an empty cell.  This board passed in to Window to set the values.
    
    Also corrected bug on layout setup as should be gridLayout = QGridLayout()  without self passed in.

--- QtSudoku.py
+++ QtSudoku.py
@@ -4,9 +4,11 @@ from PyQt5.QtWidgets import QMainWindow, QLabel, QGridLayout, QWidget
 from PyQt5.QtCore import QSize    
 
 from PyQt5.QtGui import QPainter, QBrush, QPen
+
+
      
 class SudokuMainWindow(QMainWindow):
-    def __init__(self):
+    def __init__(self, board):
         super(QMainWindow, self).__init__()
  
         self.setMinimumSize(QSize(100, 100))    
@@ -15,23 +17,40 @@ class SudokuMainWindow(QMainWindow):
         centralWidget = QWidget(self)          
         self.setCentralWidget(centralWidget)   
  
-        gridLayout = QGridLayout(self)     
+        gridLayout = QGridLayout()     
         centralWidget.setLayout(gridLayout)  
         
-        for i in range(0,9):
-            for j in range(0,9):
-                title = QLabel(str(j+1), self) 
-                title.setAlignment(QtCore.Qt.AlignCenter)
-                gridLayout.addWidget(title, i, j)
+        self.CreateBoard(board, gridLayout)
        
         menu = self.menuBar().addMenu('File')
         action = menu.addAction('Quit')
         action.triggered.connect(QtWidgets.QApplication.quit)
- 
+        
+    def CreateBoard(self, board, gridLayout):
+        for i in range(0,9):
+            for j in range(0,9):
+                cellString = str(board[i][j]) if board[i][j] is not 0 else ' '
+                cellLabel = QLabel(cellString, self) 
+                cellLabel.setAlignment(QtCore.Qt.AlignCenter)
+                gridLayout.addWidget(cellLabel, i, j) 
+                
 if __name__ == "__main__":
     def run_app():
+        
+        board = [
+        [7,8,0,4,0,0,1,2,0],
+        [6,0,0,0,7,5,0,0,9],
+        [0,0,0,6,0,1,0,7,8],
+        [0,0,7,0,4,0,2,6,0],
+        [0,0,1,0,5,0,9,3,0],
+        [9,0,4,0,6,0,0,0,5],
+        [0,7,0,3,0,0,0,1,2],
+        [1,2,0,0,0,7,4,0,0],
+        [0,4,9,2,0,6,0,0,7]
+        ]
+        
         app = QtWidgets.QApplication(sys.argv)
-        mainWin = SudokuMainWindow()
+        mainWin = SudokuMainWindow(board)
         mainWin.show()
         return app.exec_()
 
