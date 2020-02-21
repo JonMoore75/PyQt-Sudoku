eb8afeff72e5ad2692816027f86ebc5e08fdab70
Jon Moore <jonr.moore75@gmail.com>
2020-02-21 15:22:46 +0000

    Split Model and View into Separate Files + Diff BoxLine and Pointing Pairs
    Split the one long file into model and view code.
    In the new model file define more clearly the difference between a BoxLine and a Pointing Pair

--- QtSudoku.py
+++ QtSudoku.py
@@ -10,6 +10,8 @@ from PyQt5.QtGui import QFont
 
 from os import environ
 
+import Sudoku as sd
+
 ###############################################################################
 
 if hasattr(QtCore.Qt, 'AA_EnableHighDpiScaling'):
@@ -20,301 +22,7 @@ if hasattr(QtCore.Qt, 'AA_UseHighDpiPixmaps'):
     
 environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
 
-###############################################################################
-# Sudoku Model - These functions deal with the board, its candidates and 
-# solutions 
 
-def checkListForDuplicates(listOfElems):
-    """ Check if given list contains any duplicates """
-    return len(listOfElems) != len(set(listOfElems))
-
-def FindRowDuplicates(board):
-    """ If any duplicates numbers by row return location as (i,j) pairs """
-    duplicates= []
-    
-    for i in range(0,9):
-        row_i = board[i]
-        for n in range(1,10):
-            if row_i.count(n) > 1:
-                duplicates += [(i,j) for j,x in enumerate(board[i]) if x == n]
-        
-    return duplicates
-            
-def FindColDuplicates(board):
-    """ If any duplicates numbers by column return location as (i,j) pairs """    
-    duplicates= []
-    
-    for j in range(0,9):
-        col_j = [row[j] for row in board]
-        for n in range(1,10):
-            if col_j.count(n) > 1:
-                duplicates += [(i,j) for i,x in enumerate(col_j) if x == n]
-    
-    return duplicates
-    
-def FindBlockDuplicates(board):
-    """ If any duplicates numbers by block return location as (i,j) pairs """
-    duplicates= []
-    
-    for b in range(0,9):
-        bi, bj = b/3, b%3
-        block = [board[bi*3 + ci][bj*3 + cj] for ci in range(0,3) for cj in range(0,3)]
-
-        for n in range(1,10):
-            if block.count(n) > 1:
-                duplicates += [(3*bi+k/3,3*bj+k%3) for k,x in enumerate(block) if x == n]
-            
-    return duplicates
-
-def FindDuplicates(board):
-    """ Find duplicate entries on any row, column or block """
-    return FindRowDuplicates(board) + \
-        FindColDuplicates(board) + \
-        FindBlockDuplicates(board)  
-        
-def CheckValid(dup):
-    """ Pass in list of duplicates via dup (output of FindDuplicates()).  If 
-    no duplicates then the board is valid.  This does not indicate if the 
-    board has a unique solution """
-    return len(dup) == 0
-
-def RemoveZeros(inputList):
-    """ Remove zeros from a list, returns only non-zero values """
-    return list(filter(lambda a: a != 0, inputList))
-
-def GetCandidateList(board, i, j):
-    """ Find possible values in a cell """
-    bi = i/3
-    bj = j/3
-    
-    block = [[board[bi*3 + ci][bj*3 + cj] for cj in range(0,3)] for ci in range(0,3)]
-    row_i = board[i]
-    col_j = [row[j] for row in board]
-    
-    blockValues = set(RemoveZeros([c for row in block for c in row]))
-    rowValues = set(RemoveZeros(row_i))
-    colValues = set(RemoveZeros(col_j))
-    
-    return set(range(1,10)) - blockValues - rowValues - colValues
-
-def FillSingleCandidates(board, candBoard):
-    """ Fills in any empty cells with single candidate 
-    Copies inputs so not changed by this function."""
-    boardcopy = deepcopy(board)
-    changed = False
-    
-    for i in range(0,9):
-        for j in range(0,9): 
-            if len(candBoard[i][j]) == 1 and board[i][j] == 0:
-                changed = True
-                boardcopy[i][j] = next(iter(candBoard[i][j]))
-    
-    return changed, boardcopy, SolveCandidates(boardcopy)
-
-def SolveCandidates(board):
-    """ Takes a Sudoku board (2d 9x9 list of ints with 0 as empty cell) and 
-    returns a board that is a 2d 9x9 list of sets.  Each set is the possible 
-    int values. Known values are now sets with 1 item. """
-    candBoard = deepcopy(board)
-    for i in range(0,9):
-        for j in range(0,9):
-            if board[i][j] == 0:
-                candBoard[i][j] = GetCandidateList(board, i, j)
-            else:
-                candBoard[i][j] = set([board[i][j]])
-    return candBoard
-
-def FindEmpty(board):
-    """ Find the first cell that is unknown empty """
-    for i in range(0,9):
-        for j in range(0,9):
-            if board[i][j] == 0:
-                return (i,j)
-    return None
-
-def SolvewBacktrack(board):
-    """ Solve the puzzle via the backtracking algorithm """
-    foundEmpty = FindEmpty(board)
-    
-    if not foundEmpty:
-        return True # Solved!
-    
-    i,j = foundEmpty
-    candSet = GetCandidateList(board, i, j)
-    
-    for cand in iter(candSet):
-        
-        board[i][j] = cand
-        
-        if SolvewBacktrack(board):
-            return True
-        
-        board[i][j] = 0
-        
-    return False
-
-def HiddenSingles(board, candBoard):
-    """ Find hidden singles, ie situations where a number has only one cell 
-    it can go in on a row, column or block """
-    values = []
-    
-    # Rows
-    for i in range(0,9):
-        row_i = board[i]
-        c_row_i = candBoard[i]
-        
-        for n in range(1,10):
-            count = 0
-            idx = None
-            for j in range(0,9):
-                if row_i[j] == 0 and n in c_row_i[j]:
-                    count += 1
-                    idx = j
-            
-            if count == 1:
-                values += [(i,idx,n)]
-    # Columns         
-    for j in range(0,9):
-        col_j = [row[j] for row in board]
-        c_col_j = [row[j] for row in candBoard]
-        
-        for n in range(1,10):
-            count = 0
-            idx = None
-            for i in range(0,9):
-                if col_j[i] == 0 and n in c_col_j[i]:
-                    count += 1
-                    idx = i
-            
-            if count == 1:
-                values += [(idx,j,n)]
-    #Block
-    for b in range(0,9):
-        bi, bj = b/3, b%3
-        block = [board[bi*3 + ci][bj*3 + cj] for ci in range(0,3) for cj in range(0,3)]
-        c_block = [candBoard[bi*3 + ci][bj*3 + cj] for ci in range(0,3) for cj in range(0,3)]
-
-        for n in range(1,10):
-            count = 0
-            idx = None
-            for k in range(0,9):
-                if block[k] == 0 and n in c_block[k]:
-                    count += 1
-                    idx = k
-                    
-            if count == 1:
-                values += [(3*bi + idx / 3, 3*bj + idx % 3, n)]    
-                
-    return values
-
-
-def PointingPairs(board, candBoard):
-    values = []
-    
-    # Rows
-    for i in range(0,9):
-        row_i = board[i]
-        c_row_i = candBoard[i]
-        
-        for n in range(1,10):
-            count = 0
-            idx = []
-            for j in range(0,9):
-                if row_i[j] == 0 and n in c_row_i[j]:
-                    count += 1
-                    idx += [j]
-            
-            if count == 2:
-                j1, j2 = idx[0], idx[1]
-                if j1/3 == j2/3:
-                    values += [(n, i, j1, i, j2)]
-                    
-    # Columns         
-    for j in range(0,9):
-        col_j = [row[j] for row in board]
-        c_col_j = [row[j] for row in candBoard]
-        
-        for n in range(1,10):
-            count = 0
-            idx = []
-            for i in range(0,9):
-                if col_j[i] == 0 and n in c_col_j[i]:
-                    count += 1
-                    idx += [i]
-            
-            if count == 2:
-                i1, i2 = idx[0], idx[1]
-                if i1/3 == i2/3:
-                    values += [(n, i1, j, i2, j)]
-    
-    return values
-    
-def Pairs(board, candBoard):
-    values = []
-    
-    # Rows
-    for i in range(0,9):
-        pairs = []
-        pair_loc = []
-        
-        c_row_i = candBoard[i]
-        
-        for j in range(0,9):
-            if len(c_row_i[j]) == 2:
-                it = iter(c_row_i[j])
-                a,b = next(it), next(it)
-                
-                for k, pair in enumerate(pairs):
-                    if (a,b) == pair:
-                        values += [(a,b, i, pair_loc[k], i, j)]
-                
-                pairs += [(a,b)]
-                pair_loc += [j]
-                
-    # Columnss
-    for j in range(0,9):
-        pairs = []
-        pair_loc = []
-        
-        c_col_j = [row[j] for row in candBoard]
-        
-        for i in range(0,9):
-            if len(c_col_j[i]) == 2:
-                it = iter(c_col_j[i])
-                a,b = next(it), next(it)
-                
-                for k, pair in enumerate(pairs):
-                    if (a,b) == pair:
-                        values +=[(a,b, pair_loc[k], j, i, j)]
-                
-                pairs += [(a,b)]
-                pair_loc += [i]
-                
-    # Blocks
-    for b in range(0,9):
-        pairs = []
-        pair_loc = []
-        
-        bi, bj = b/3, b%3
-        c_block = [candBoard[bi*3 + ci][bj*3 + cj] for ci in range(0,3) for cj in range(0,3)]
-
-        for k in range(0,9):
-            if len(c_block[k]) == 2:
-                it = iter(c_block[k])
-                a,b = next(it), next(it)
-                i,j = 3*bi + k / 3, 3*bj + k % 3
-                
-                print a,b, i, j
-                
-                for k, pair in enumerate(pairs):
-                    if (a,b) == pair:
-                        values += [(a,b, pair_loc[k][0], pair_loc[k][1], i, j)]
-                
-                pairs += [(a,b)]
-                pair_loc += [(i,j)]
-            
-    return values
-            
     
 ###############################################################################
 # View - this is the pyQT5 GUI dealing with input and display
@@ -632,19 +340,19 @@ class SudokuMainWindow(QMainWindow):
  
     def Solve(self):
         """ Solves the board using the backtracking alogorithm """
-        dups = FindDuplicates(self.currBoard)
-        if CheckValid(dups):
+        dups = sd.FindDuplicates(self.currBoard)
+        if sd.CheckValid(dups):
             prevBoard = deepcopy(self.currBoard)
             
-            solved = SolvewBacktrack(self.currBoard)
+            solved = sd.SolvewBacktrack(self.currBoard)
             
             if not solved:
                 print 'No solution'
             else:
                 self.UpdateChangedCells(prevBoard)
                 
-                dups = FindDuplicates(self.currBoard)
-                if not CheckValid(dups):
+                dups = sd.FindDuplicates(self.currBoard)
+                if not sd.CheckValid(dups):
                     print 'Invalid'
                     
         self.ShowInvalidCells(dups)
@@ -652,16 +360,16 @@ class SudokuMainWindow(QMainWindow):
     def FillinSingleCandidatesStep(self):
         """ Look for cells with only 1 candidate and fill them in.
         Updates the candidates after finished """
-        dups = FindDuplicates(self.currBoard)
-        if CheckValid(dups):
+        dups = sd.FindDuplicates(self.currBoard)
+        if sd.CheckValid(dups):
             prevBoard = deepcopy(self.currBoard)
             
-            changed, self.currBoard, self.candBoard = FillSingleCandidates(self.currBoard, self.candBoard)
+            changed, self.currBoard, self.candBoard = sd.FillSingleCandidates(self.currBoard, self.candBoard)
             if changed:
                 self.UpdateChangedCells(prevBoard)
             
-            dups = FindDuplicates(self.currBoard)
-            if not CheckValid(dups):
+            dups = sd.FindDuplicates(self.currBoard)
+            if not sd.CheckValid(dups):
                 print 'Invalid'
                 
         self.ShowInvalidCells(dups)
@@ -670,19 +378,19 @@ class SudokuMainWindow(QMainWindow):
     def FillinSingleCandidates(self):
         """ Look for cells with only 1 candidate and fill them in 
         Then update candidates and iterate until no more changes """
-        dups = FindDuplicates(self.currBoard)
-        if CheckValid(dups):
+        dups = sd.FindDuplicates(self.currBoard)
+        if sd.CheckValid(dups):
             notdone = True
             
             prevBoard = deepcopy(self.currBoard)
             
             while notdone:
-                notdone, self.currBoard, self.candBoard = FillSingleCandidates(self.currBoard, self.candBoard)
+                notdone, self.currBoard, self.candBoard = sd.FillSingleCandidates(self.currBoard, self.candBoard)
     
             self.UpdateChangedCells(prevBoard)
                         
-            dups = FindDuplicates(self.currBoard)
-            if not CheckValid(dups):
+            dups = sd.FindDuplicates(self.currBoard)
+            if not sd.CheckValid(dups):
                 print 'Invalid'
                 
         self.ShowInvalidCells(dups)
@@ -694,7 +402,7 @@ class SudokuMainWindow(QMainWindow):
         
     def HighlightHiddenSingles(self):
         self.ClearHighlights()
-        hiddenSingles = HiddenSingles(self.currBoard, self.candBoard)
+        hiddenSingles = sd.HiddenSingles(self.currBoard, self.candBoard)
                 
         for hiddenSingle in hiddenSingles:
             i,j,n = hiddenSingle
@@ -702,7 +410,7 @@ class SudokuMainWindow(QMainWindow):
             
     def HighlightNakedPairs(self):
         self.ClearHighlights()
-        pairSets = Pairs(self.currBoard, self.candBoard)
+        pairSets = sd.NakedPairs(self.currBoard, self.candBoard)
         
         for pairSet in pairSets:
             a,b,i1,j1,i2,j2 = pairSet
@@ -712,7 +420,7 @@ class SudokuMainWindow(QMainWindow):
     def HighlightPointingPairs(self):
         self.ClearHighlights()
         
-        pairSets = PointingPairs(self.currBoard, self.candBoard)
+        pairSets = sd.PointingPairs(self.currBoard, self.candBoard)
         
         for pairSet in pairSets:
             n,i1,j1,i2,j2 = pairSet
@@ -723,7 +431,7 @@ class SudokuMainWindow(QMainWindow):
     def RegenerateCandidates(self):
         """ Reset the displayed candidates to those based on those that are 
         valid (ie avoid duplicates)"""
-        self.candBoard = SolveCandidates(self.currBoard)
+        self.candBoard = sd.SolveCandidates(self.currBoard)
         
         for i in range(0,9):
             for j in range(0,9):
@@ -750,15 +458,15 @@ class SudokuMainWindow(QMainWindow):
             if QtCore.Qt.Key_1 <= key <= QtCore.Qt.Key_9:                    
                 self.selectedCell.UpdateValue(keyStr)
                 self.currBoard[self.selectedCell.i][self.selectedCell.j] = int(keyStr)
-                self.candBoard = SolveCandidates(self.currBoard)
+                self.candBoard = sd.SolveCandidates(self.currBoard)
                     
             if key == QtCore.Qt.Key_Backspace:
                 self.selectedCell.UpdateValue(' ')
                 self.currBoard[self.selectedCell.i][self.selectedCell.j] = 0
-                self.candBoard = SolveCandidates(self.currBoard)
+                self.candBoard = sd.SolveCandidates(self.currBoard)
             
-            dups = FindDuplicates(self.currBoard)
-            if not CheckValid(dups):
+            dups = sd.FindDuplicates(self.currBoard)
+            if not sd.CheckValid(dups):
                 print 'Invalid', dups
             
             self.ShowInvalidCells(dups)
@@ -770,9 +478,9 @@ class SudokuMainWindow(QMainWindow):
 
 def run_app(origBoard):
     """ Main application function """
-    print 'Board is valid:', CheckValid(FindDuplicates(origBoard))
+    print 'Board is valid:', sd.CheckValid(sd.FindDuplicates(origBoard))
     
-    candBoard = SolveCandidates(origBoard)
+    candBoard = sd.SolveCandidates(origBoard)
 
     app = QtWidgets.QApplication(sys.argv)
     mainWin = SudokuMainWindow(origBoard, candBoard)
@@ -798,14 +506,17 @@ def UnitTests():
     ]
     
     testboard[4][4] = 3    
-    print 'Check valid via Row Duplicate Test. Should be False', CheckValid(FindDuplicates(testboard))
+    print 'Check valid via Row Duplicate Test. Should be False', \
+        sd.CheckValid(sd.FindDuplicates(testboard))
     
     testboard[4][4] = 7
-    print 'Check valid via Col Duplicate Test. Should be False', CheckValid(FindDuplicates(testboard))
+    print 'Check valid via Col Duplicate Test. Should be False', \
+        sd.CheckValid(sd.FindDuplicates(testboard))
     
     testboard[4][4] = 5
     testboard[8][8] = 1
-    print 'Check valid via Block Duplicate Test. Should be False', CheckValid(FindDuplicates(testboard))
+    print 'Check valid via Block Duplicate Test. Should be False', \
+        sd.CheckValid(sd.FindDuplicates(testboard))
     
     print '######### End Unit Tests #############'
 
---
+++ Sudoku.py
@@ -0,0 +1,319 @@
+from copy import deepcopy
+
+###############################################################################
+# Sudoku Model - These functions deal with the board, its candidates and 
+# solutions 
+
+def checkListForDuplicates(listOfCells):
+    """ Check if given list contains any duplicates """
+    return len(listOfCells) != len(set(listOfCells))
+
+def FindRowDuplicates(board):
+    """ If any duplicates numbers by row return location as (i,j) pairs """
+    duplicates= []
+    
+    for i in range(0,9):
+        row_i = board[i]
+        for n in range(1,10):
+            if row_i.count(n) > 1:
+                duplicates += [(i,j) for j,x in enumerate(board[i]) if x == n]
+        
+    return duplicates
+            
+def FindColDuplicates(board):
+    """ If any duplicates numbers by column return location as (i,j) pairs """    
+    duplicates= []
+    
+    for j in range(0,9):
+        col_j = [row[j] for row in board]
+        for n in range(1,10):
+            if col_j.count(n) > 1:
+                duplicates += [(i,j) for i,x in enumerate(col_j) if x == n]
+    
+    return duplicates
+    
+def FindBlockDuplicates(board):
+    """ If any duplicates numbers by block return location as (i,j) pairs """
+    duplicates= []
+    
+    for b in range(0,9):
+        bi, bj = b/3, b%3
+        block = [board[bi*3 + ci][bj*3 + cj] for ci in range(0,3) for cj in range(0,3)]
+
+        for n in range(1,10):
+            if block.count(n) > 1:
+                duplicates += [(3*bi+k/3,3*bj+k%3) for k,x in enumerate(block) if x == n]
+            
+    return duplicates
+
+def FindDuplicates(board):
+    """ Find duplicate entries on any row, column or block """
+    return FindRowDuplicates(board) + \
+        FindColDuplicates(board) + \
+        FindBlockDuplicates(board)  
+        
+def CheckValid(dup):
+    """ Pass in list of duplicates via dup (output of FindDuplicates()).  If 
+    no duplicates then the board is valid.  This does not indicate if the 
+    board has a unique solution """
+    return len(dup) == 0
+
+###############################################################################
+
+def RemoveZeros(inputList):
+    """ Remove zeros from a list, returns only non-zero values """
+    return list(filter(lambda a: a != 0, inputList))
+
+def GetCandidateList(board, i, j):
+    """ Find possible values in a cell """
+    bi = i/3
+    bj = j/3
+    
+    block = [[board[bi*3 + ci][bj*3 + cj] for cj in range(0,3)] for ci in range(0,3)]
+    row_i = board[i]
+    col_j = [row[j] for row in board]
+    
+    blockValues = set(RemoveZeros([c for row in block for c in row]))
+    rowValues = set(RemoveZeros(row_i))
+    colValues = set(RemoveZeros(col_j))
+    
+    return set(range(1,10)) - blockValues - rowValues - colValues
+
+def FillSingleCandidates(board, candBoard):
+    """ Fills in any empty cells with single candidate 
+    Copies inputs so not changed by this function."""
+    boardcopy = deepcopy(board)
+    changed = False
+    
+    for i in range(0,9):
+        for j in range(0,9): 
+            if len(candBoard[i][j]) == 1 and board[i][j] == 0:
+                changed = True
+                boardcopy[i][j] = next(iter(candBoard[i][j]))
+    
+    return changed, boardcopy, SolveCandidates(boardcopy)
+
+def SolveCandidates(board):
+    """ Takes a Sudoku board (2d 9x9 list of ints with 0 as empty cell) and 
+    returns a board that is a 2d 9x9 list of sets.  Each set is the possible 
+    int values. Known values are now sets with 1 item. """
+    candBoard = deepcopy(board)
+    for i in range(0,9):
+        for j in range(0,9):
+            if board[i][j] == 0:
+                candBoard[i][j] = GetCandidateList(board, i, j)
+            else:
+                candBoard[i][j] = set([board[i][j]])
+    return candBoard
+###############################################################################
+
+def FindEmpty(board):
+    """ Find the first cell that is unknown empty """
+    for i in range(0,9):
+        for j in range(0,9):
+            if board[i][j] == 0:
+                return (i,j)
+    return None
+
+def SolvewBacktrack(board):
+    """ Solve the puzzle via the backtracking algorithm """
+    foundEmpty = FindEmpty(board)
+    
+    if not foundEmpty:
+        return True # Solved!
+    
+    i,j = foundEmpty
+    candSet = GetCandidateList(board, i, j)
+    
+    for cand in iter(candSet):
+        
+        board[i][j] = cand
+        
+        if SolvewBacktrack(board):
+            return True
+        
+        board[i][j] = 0
+        
+    return False
+
+###############################################################################
+
+def HiddenSingles(board, candBoard):
+    """ Find hidden singles, ie situations where a number has only one cell 
+    it can go in on a row, column or block """
+    values = []
+    
+    # Rows
+    for i in range(0,9):
+        row_i = board[i]
+        c_row_i = candBoard[i]
+        
+        for n in range(1,10):
+            count = 0
+            idx = None
+            for j in range(0,9):
+                if row_i[j] == 0 and n in c_row_i[j]:
+                    count += 1
+                    idx = j
+            
+            if count == 1:
+                values += [(i,idx,n)]
+    # Columns         
+    for j in range(0,9):
+        col_j = [row[j] for row in board]
+        c_col_j = [row[j] for row in candBoard]
+        
+        for n in range(1,10):
+            count = 0
+            idx = None
+            for i in range(0,9):
+                if col_j[i] == 0 and n in c_col_j[i]:
+                    count += 1
+                    idx = i
+            
+            if count == 1:
+                values += [(idx,j,n)]
+    #Block
+    for b in range(0,9):
+        bi, bj = b/3, b%3
+        block = [board[bi*3 + ci][bj*3 + cj] for ci in range(0,3) for cj in range(0,3)]
+        c_block = [candBoard[bi*3 + ci][bj*3 + cj] for ci in range(0,3) for cj in range(0,3)]
+
+        for n in range(1,10):
+            count = 0
+            idx = None
+            for k in range(0,9):
+                if block[k] == 0 and n in c_block[k]:
+                    count += 1
+                    idx = k
+                    
+            if count == 1:
+                values += [(3*bi + idx / 3, 3*bj + idx % 3, n)]    
+                
+    return values
+
+def RowCells(board, i):
+    return board[i]
+
+def ColCells(board, j):
+    return [row[j] for row in board]
+
+def BlockCells(board, b):
+    bi, bj = b/3, b%3
+    return [board[bi*3 + ci][bj*3 + cj] for ci in range(0,3) for cj in range(0,3)]
+
+def BlockCoords(b,k):
+    bi, bj = b/3, b%3
+    return 3*bi + k / 3, 3*bj + k % 3
+
+def FindNakedPair(candBoard, ElemFunc, CoordFunc):
+    values = []
+    
+    # Search through 9 cell element (row, column or block)
+    for e in range(0,9):
+        pairs = []
+        pair_loc = []
+        
+        cells = ElemFunc(candBoard, e) 
+        
+        # Loop through each cell of element see if any pairs (cells with 2 cands)
+        for c in range(0,9):
+            if len(cells[c]) == 2:
+                # Get the values of the pair (a,b) and record location (i,j)
+                it = iter(cells[c])
+                a,b = next(it), next(it)
+                i,j = CoordFunc(e, c)
+                
+                # If match previously found pair we have a Naked Pair
+                for p, pair in enumerate(pairs):
+                    i1, j1 = pair_loc[p][0], pair_loc[p][1]
+                    if (a,b) == pair:
+                        values += [(a,b, i1, j1, i, j)]
+                
+                # Remember this pair
+                pairs += [(a,b)]
+                pair_loc += [(i,j)]
+            
+    return values
+
+def NakedPairs(board, candBoard):
+    # Rows
+    values = FindNakedPair(candBoard, RowCells, lambda b,k: (b,k))
+                
+    # Columns
+    values += FindNakedPair(candBoard, ColCells, lambda b,k: (k,b))
+                
+    # Blocks
+    values += FindNakedPair(candBoard, BlockCells, BlockCoords)
+            
+    return values
+
+def FindBoxLinePair(board, candBoard, ElemFunc, isRow):
+    values = []
+    
+    # Search through 9 cell element (row, column or block)
+    for e in range(0,9):
+        cells = ElemFunc(board, e) 
+        candsInElem = ElemFunc(candBoard, e) 
+        
+        # Loop through all possible numbers
+        for n in range(1,10):
+            count = 0
+            idx = []
+            
+            # Loop through each cell of the element
+            for c in range(0,9):
+                if cells[c] == 0 and n in candsInElem[c]:
+                    count += 1
+                    idx += [c]
+            
+            # If only found number n twice in col or row, check if in same block
+            if count == 2:
+                if idx[0]/3 == idx[1]/3:
+                    if isRow:
+                        i1, j1, i2, j2 = e, idx[0], e, idx[1] 
+                    else:
+                        i1, j1, i2, j2 = idx[0], e, idx[1], e
+                    values += [(n, i1, j1, i2, j2)]
+    return values    
+    
+def BoxLinePairs(board, candBoard):
+    values = FindBoxLinePair(board, candBoard, RowCells, isRow=True)
+    values += FindBoxLinePair(board, candBoard, ColCells, isRow=False)
+    
+    return values
+
+def FindPointingPair(board, candBoard, ElemFunc, isRow):
+    values = []
+    
+    # Loop through each block
+    for b in range(0,9):
+        cellsInBlock = BlockCells(board, b)
+        candsInBlock = BlockCells(candBoard, b)
+        
+        # Loop through all possible numbers
+        for n in range(1,10):
+            count = 0
+            idx = []
+            
+            # Loop through each cell of the block
+            for c in range(0,9):
+                if cellsInBlock[c] == 0 and n in candsInBlock[c]:
+                    count += 1
+                    idx += [c]
+
+            # If only found number n twice in col or row, check if in same block
+            if count == 2:
+                i1,j1 = idx[0] / 3, idx[0] % 3
+                i2,j2 = idx[1] / 3, idx[1] % 3
+                
+                if i1 == i2 or j1 == j2:
+                    values += [(n, i1, j1, i2, j2)]
+                    
+    return values    
+
+def PointingPairs(board, candBoard):
+    values = FindPointingPair(board, candBoard, RowCells, isRow=True)
+    values += FindPointingPair(board, candBoard, ColCells, isRow=False)
+    
+    return values
